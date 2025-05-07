a27856f7fb86a8bbb966a99f660b480ed70797e7
DiegoAlexander23senati <diegodianderas23@gmail.com>
2025-05-07 09:29:15 -0500

    se actualizo programs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using calculadora.Logica;
+
+Calculadora cal=new();
+Console.WriteLine("Ingrese N1");
+int n1=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese N2");
+int n2=int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese :SUMA S, RESTA R, MULTIPLICACION M, DIVISION D");
+string opcion=Console.ReadLine();
+int res=opcion=="s"?cal.sumar(n1 ,n2):opcion=="r"?cal.restar(n1 ,n2):opcion=="m"?cal.multiplicar(n1 ,n2):opcion=="d"?cal.dividir(n1 ,n2):0;
