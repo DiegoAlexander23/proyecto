f8a00dd7a9b067454aca55c8673d8c1c2dcf095d
Piero <piero10.5.rodrigo@hotmail.com>
2025-05-07 09:20:18 -0500

    Subida de la carpeta Logica con la multiplicacion y division

---
+++ Logica/Calculadora.cs
@@ -0,0 +1,8 @@
+namespace proyecto.Logica
+{
+    public class Calculadora
+    {
+        public int Multiplicar(int n1 ,int n2)=> n1*n2;
+        public int Dividir(int n1 ,int n2)=> n1/n2;
+    }
+}
